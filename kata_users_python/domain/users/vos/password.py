6ca9c07ec9db0235783b1f52c7a0e277d1ec144d
Nicolas Rios <dnicolas.rios98@gmail.com>
2024-11-03 18:47:26 +0100

    fix: fix password regex

--- kata_users_python/domain/users/vos/password.py
+++ kata_users_python/domain/users/vos/password.py
@@ -1,7 +1,11 @@
+import re
+
 from pydantic import ConfigDict, Field, RootModel
+
+PASSWORD_REGEX = re.compile(r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$")
 
 
 class Password(RootModel):
-    root: str = Field(min_length=8, pattern=r"/^(?=.*[A-Za-z])(?=.*\d).{8,}$/")
+    root: str = Field(min_length=8, pattern=PASSWORD_REGEX)
 
-    model_config = ConfigDict(frozen=True)
+    model_config = ConfigDict(regex_engine="python-re", frozen=True)
