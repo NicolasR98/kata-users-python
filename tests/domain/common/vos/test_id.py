cad19bf4327ecf0eee6d27d601c1e011bcf66073
Nicolas Rios <dnicolas.rios98@gmail.com>
2024-11-03 18:48:24 +0100

    chore: add tests for id VO

--- tests/domain/common/vos/test_id.py
+++ tests/domain/common/vos/test_id.py
@@ -1,6 +1,23 @@
+import uuid
+
+import pytest
+
 from kata_users_python.domain.common.vos.id import Id
 
 
 class TestId:
-    def test_valid_arguments(self):
+    @pytest.fixture
+    def raw_id(self):
+        return uuid.uuid4()
+
+    def test_valid_arguments(self, raw_id):
+        assert Id(raw_id)
+
+    def test_no_arguments(self):
         assert Id()
+
+    def test_equality_same(self, raw_id):
+        assert Id(raw_id) == Id(raw_id)
+
+    def test_equality_different(self):
+        assert Id() != Id()
